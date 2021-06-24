a59abe32e05e3d6ae01433354ec5117a03daed18
David Bouscarle <d.bouscarle@gmail.com>
2021-06-24 10:13:41 +0200

    "Create todo_app database; Create Task.php, migrations done"

--- src/Entity/TodoList.php
+++ src/Entity/TodoList.php
@@ -1,5 +1,7 @@
 <?php
 namespace App\Entity;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -25,10 +27,20 @@ class TodoList
 
     /**
      * @Assert\NotBlank(message="Veuillez renseigner la couleur de la liste")
-     * @Assert\Length(min="2", max="80", minMessage="La couleur saisie est trop court", maxMessage="La couleur saisie est trop long")
-     * @ORM\Column(type="string", length="80")
+     * @Assert\Length(min="2", max="10", minMessage="La couleur saisie est trop court", maxMessage="La couleur saisie est trop long")
+     * @ORM\Column(type="string", length="10")
      */
     private string $color;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Task::class, mappedBy="list", orphanRemoval=true)
+     */
+    private $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new ArrayCollection();
+    }
 
     /**
      * @return string
@@ -81,6 +93,36 @@ class TodoList
     public function setId(int $id): TodoList
     {
         $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return Collection|Task[]
+     */
+    public function getTasks(): Collection
+    {
+        return $this->tasks;
+    }
+
+    public function addTask(Task $task): self
+    {
+        if (!$this->tasks->contains($task)) {
+            $this->tasks[] = $task;
+            $task->setList($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTask(Task $task): self
+    {
+        if ($this->tasks->removeElement($task)) {
+            // set the owning side to null (unless already changed)
+            if ($task->getList() === $this) {
+                $task->setList(null);
+            }
+        }
+
         return $this;
     }
 }
